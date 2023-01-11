90c6b506c48b3a1f9d262b28c246be1edceee554
Yuri UTKIN <yu-2023@inbox.lv>
2023-01-11 13:28:30 +0200

    Add files via upload

--- src/main/kotlin/lv/yu/kot/KOT_04_menu_wrap.kt
+++ src/main/kotlin/lv/yu/kot/KOT_04_menu_wrap.kt
@@ -3,9 +3,9 @@
  * 
  * Kotlin package     lv.yu.kot
  * 
- * Kotlin program     KOT_03_menu.kt     Apache License 2.0
+ * Kotlin program     KOT_04_menu_wrap.kt     Apache License 2.0
  *      
- * Copyright (c)      Yuri Utkin 2023    mob.+371 12345678     https://www.jago.lv
+ * Copyright (c)      Yuri Utkin 2023         mob.+371 12345678     https://www.jago.lv
  * 
  */
 package lv.yu.kot
@@ -28,20 +28,20 @@ import java.awt.event.ActionEvent
 
 //------------------------------
 
-fun KOT_03_menu() {
+fun KOT_04_menu_wrap() {
 	
     var imgURL     =           Thread.currentThread().getContextClassLoader().getResource("lv/yu/kot/KOT_resources/KOT.jpg")  
     var icon_frame = ImageIcon(Thread.currentThread().getContextClassLoader().getResource("lv/yu/kot/KOT_resources/KOT.jpg"))
 
 //------------------------------	
 
-    var title = "KOTLIN -- KOT_03_menu -- v. 2023.01.01"
+    var title = "KOTLIN -- KOT_04_menu_wrap -- v. 2023.01.01"
 
     val frame = JFrame()
     
         frame.setTitle("     " + title)
     
-        frame.setBounds(10+2*40+150, 10+2*40, 1000, 600)
+        frame.setBounds(10+3*40+150, 10+3*40, 1000, 600)
         
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
         
@@ -52,52 +52,52 @@ fun KOT_03_menu() {
 //------------------------------
 
     val icon_Start       = ImageIcon(Thread.currentThread().contextClassLoader.getResource("lv/yu/kot/KOT_resources/start.gif"))
-    val icon_Load        = ImageIcon(Thread.currentThread().contextClassLoader.getResource("lv/yu/kot/KOT_resources/load.gif"))
-    val icon_Xxxx        = ImageIcon(Thread.currentThread().contextClassLoader.getResource("lv/yu/kot/KOT_resources/xxxx.png"))
-    val icon_Information = ImageIcon(Thread.currentThread().contextClassLoader.getResource("lv/yu/kot/KOT_resources/information.gif"))
-    val icon_Help        = ImageIcon(Thread.currentThread().contextClassLoader.getResource("lv/yu/kot/KOT_resources/help.png"))
-    val icon_About       = ImageIcon(Thread.currentThread().contextClassLoader.getResource("lv/yu/kot/KOT_resources/about.gif"))
-    val icon_Eng         = ImageIcon(Thread.currentThread().contextClassLoader.getResource("lv/yu/kot/KOT_resources/prefs.gif"))
-    val icon_Rus         = ImageIcon(Thread.currentThread().contextClassLoader.getResource("lv/yu/kot/KOT_resources/prefs.gif"))
-    val icon_Lat         = ImageIcon(Thread.currentThread().contextClassLoader.getResource("lv/yu/kot/KOT_resources/prefs.gif"))
+    val icon_Load        = ImageIcon(Thread.currentThread().contextClassLoader.getResource("lv/yu/kot/KOT_resources/load.gif"))    
+    val icon_Xxxx        = ImageIcon(Thread.currentThread().contextClassLoader.getResource("lv/yu/kot/KOT_resources/xxxx.png"))    
+    val icon_Information = ImageIcon(Thread.currentThread().contextClassLoader.getResource("lv/yu/kot/KOT_resources/information.gif"))    
+    val icon_Help        = ImageIcon(Thread.currentThread().contextClassLoader.getResource("lv/yu/kot/KOT_resources/help.png"))    
+    val icon_About       = ImageIcon(Thread.currentThread().contextClassLoader.getResource("lv/yu/kot/KOT_resources/about.gif"))    
+    val icon_Eng         = ImageIcon(Thread.currentThread().contextClassLoader.getResource("lv/yu/kot/KOT_resources/prefs.gif"))    
+    val icon_Rus         = ImageIcon(Thread.currentThread().contextClassLoader.getResource("lv/yu/kot/KOT_resources/prefs.gif"))    
+    val icon_Lat         = ImageIcon(Thread.currentThread().contextClassLoader.getResource("lv/yu/kot/KOT_resources/prefs.gif"))    
     val icon_Exit        = ImageIcon(Thread.currentThread().contextClassLoader.getResource("lv/yu/kot/KOT_resources/exit.png"))
         
 //------------------------------
-        
+            
     val menubar = JMenuBar()
 
     val menu_Menu = JMenu("Menu")
       val item_Start = JMenuItem("Start", icon_Start)
       val item_Load  = JMenuItem("Loads", icon_Load)        
       val item_Xxxx  = JMenuItem("Xxxx", icon_Xxxx)
-	  
+
     val menu_Start1 = JMenu("Start1")  
       val item_St11 = JMenuItem("St11")
       val item_St12 = JMenuItem("St12")
-	  
+  
     val menu_Start2 = JMenu("Start2")
       val item_St21 = JMenuItem("St21")
       val item_St22 = JMenuItem("St22")
-	  
+
     val menu_Start3 = JMenu("Start3")   
       val item_St31 = JMenuItem("St31")
       val item_St32 = JMenuItem("St32")
-	  
-    val menu_Help = JMenu("Help") 
+    
+    val menu_Help = JMenu("Help")
       val item_Information = JMenuItem("Information", icon_Information)
       val item_Help        = JMenuItem("Help", icon_Help)
       val item_About       = JMenuItem("About", icon_About)
-	  
+
     val menu_Lang = JMenu("Lang")    
       val item_Eng = JCheckBoxMenuItem("Eng", icon_Eng)
       val item_Rus = JCheckBoxMenuItem("Rus", icon_Rus)
       val item_Lat = JCheckBoxMenuItem("Lat", icon_Lat)
-	  
+
     val menu_Exit = JMenu("Exit")
       val item_Exit = JMenuItem("Exit", icon_Exit)
-		      
-//------------------------------	      
     
+//------------------------------
+      
         menu_Menu.mnemonic = KeyEvent.VK_M  //  Alt M
         menu_Menu.toolTipText = "Menu commands"
           item_Start.toolTipText = "Start application"
@@ -116,12 +116,12 @@ fun KOT_03_menu() {
         menu_Start2.toolTipText = "Start2 commands"
           item_St21.toolTipText = "St21 application"
           item_St22.toolTipText = "St22 application"
-	
+    		
         menu_Start3.mnemonic = KeyEvent.VK_3  //  Alt 3
         menu_Start3.toolTipText = "Start3 commands"
           item_St31.toolTipText = "St31 application"
           item_St32.toolTipText = "St32 application"
-	
+    		
         menu_Help.mnemonic = KeyEvent.VK_H  //  Alt H
         menu_Help.toolTipText = "Help commands"
           item_Information.toolTipText = "Information application"
@@ -138,32 +138,32 @@ fun KOT_03_menu() {
         menu_Exit.mnemonic = KeyEvent.VK_E  //  Alt E
         menu_Exit.toolTipText = "Exit commands"    
           item_Exit.toolTipText = "Exit application"
-          
+    	          
 //------------------------------          
 
         menu_Menu.add(item_Start)
         menu_Menu.add(item_Load)
         menu_Menu.add(item_Xxxx)
-	
+    		
         menu_Start1.add(item_St11)
         menu_Start1.add(item_St12)
-	
+    		
         menu_Start2.add(item_St21)
         menu_Start2.add(item_St22)
-	
+    		
         menu_Start3.add(item_St31)
         menu_Start3.add(item_St32)
-	
+    		
         menu_Help.add(item_Information)
         menu_Help.add(item_Help)
         menu_Help.add(item_About)
-	
+    		
         menu_Lang.add(item_Eng)
         menu_Lang.add(item_Rus)
         menu_Lang.add(item_Lat)
-        
+    	        
 	    menu_Exit.add(item_Exit)
-            
+    	            
         menubar.add(menu_Menu)
 	    menubar.add(menu_Start1)
 		menubar.add(menu_Start2)
@@ -172,13 +172,15 @@ fun KOT_03_menu() {
 		menubar.add(menu_Lang)
         menubar.add(menu_Exit)
         
+    	menubar.setLayout( JAV_LIB_wrap_layout (JAV_LIB_wrap_layout.LEFT, 0, 0) )
+
         frame.jMenuBar = menubar
 
 //------------------------------
 
     val textarea = JTextArea()
     val scrollpane = JScrollPane(textarea)
-    	    
+    
         textarea.setText("" +
 "        \r\n" +
 "        💞️💞️ EN\r\n" +
@@ -217,12 +219,12 @@ fun KOT_03_menu() {
 
 //------------------------------
 	    
-}  //  end KOT_03_menu()
+}  //  end KOT_04_menu_wrap()
 
 //------------------------------
 
 fun main() {
-	         EventQueue.invokeLater(::KOT_03_menu)
+	         EventQueue.invokeLater(::KOT_04_menu_wrap)
 }
 
 //------------------------------
