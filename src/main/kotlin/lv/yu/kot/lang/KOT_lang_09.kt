003e9c0b51965b2ccfe4b075d92dc09970f25276
Yuri UTKIN <yu-2023@inbox.lv>
2023-02-02 14:00:12 +0200

    Update KOT_lang_09.kt

--- src/main/kotlin/lv/yu/kot/lang/KOT_lang_09.kt
+++ src/main/kotlin/lv/yu/kot/lang/KOT_lang_09.kt
@@ -14,16 +14,16 @@ fun KOT_lang_09() {
     	
     lv.yu.kot.start.KOT_start_menu_09()
     
-	lv.yu.kot.start.KOT_start_menu_start_09()
+    lv.yu.kot.start.KOT_start_menu_start_09()
     lv.yu.kot.load.KOT_load_menu_start_09()
     lv.yu.kot.xxxxx.KOT_xxxxx_menu_start_09()
     
-	lv.yu.kot.lang.KOT_lang_menu_lang_09()
+    lv.yu.kot.lang.KOT_lang_menu_lang_09()
 	
-	lv.yu.kot.help.KOT_help_menu_help_09()
-	lv.yu.kot.lang.KOT_lang_menu_lang_09()
+    lv.yu.kot.help.KOT_help_menu_help_09()
+    lv.yu.kot.lang.KOT_lang_menu_lang_09()
     lv.yu.kot.exit.KOT_exit_menu_exit_09()
 	
-	lv.yu.kot.frame_09.setJMenuBar(lv.yu.kot.menubar_09)
+    lv.yu.kot.frame_09.setJMenuBar(lv.yu.kot.menubar_09)
 
 }  //  end KOT_lang_09()
