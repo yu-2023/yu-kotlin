4d5868dafb38e65778249d2ad2dbadb11ba13f6a
Yuri UTKIN <yu-2023@inbox.lv>
2023-02-02 13:33:20 +0200

    Add files via upload

--- src/main/kotlin/lv/yu/kot/lang/KOT_lang_visualize_08.kt
+++ src/main/kotlin/lv/yu/kot/lang/KOT_lang_visualize_08.kt
@@ -14,10 +14,10 @@ fun KOT_lang_visualize_08() {
 
 //--------------------------------------------------    	
     	
-    	lv.yu.kot.lang.KOT_lang_08()
+        lv.yu.kot.lang.KOT_lang_08()
 		     		
 //--------------------------------------------------          
 			            		
-	lv.yu.kot.frame_08.isVisible = true
+        lv.yu.kot.frame_08.isVisible = true
 
 }  //  end KOT_lang_visualize_08()
