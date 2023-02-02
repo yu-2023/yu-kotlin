66d74c25056a5939c8f4b7ffdd2597448ab49deb
Yuri UTKIN <yu-2023@inbox.lv>
2023-02-02 14:02:08 +0200

    Update KOT_lang_10.kt

--- src/main/kotlin/lv/yu/kot/lang/KOT_lang_10.kt
+++ src/main/kotlin/lv/yu/kot/lang/KOT_lang_10.kt
@@ -14,16 +14,16 @@ fun KOT_lang_10() {
     	
     lv.yu.kot.start.KOT_start_menu_10()
     
-	lv.yu.kot.start.KOT_start_menu_start_10()
+    lv.yu.kot.start.KOT_start_menu_start_10()
     lv.yu.kot.load.KOT_load_menu_start_10()
     lv.yu.kot.xxxxx.KOT_xxxxx_menu_start_10()
     
-	lv.yu.kot.lang.KOT_lang_menu_lang_10()
+    lv.yu.kot.lang.KOT_lang_menu_lang_10()
 	
-	lv.yu.kot.help.KOT_help_menu_help_10()
-	lv.yu.kot.lang.KOT_lang_menu_lang_10()
+    lv.yu.kot.help.KOT_help_menu_help_10()
+    lv.yu.kot.lang.KOT_lang_menu_lang_10()
     lv.yu.kot.exit.KOT_exit_menu_exit_10()
 	
-	lv.yu.kot.frame_10.setJMenuBar(lv.yu.kot.menubar_10)
+    lv.yu.kot.frame_10.setJMenuBar(lv.yu.kot.menubar_10)
 
 }  //  end KOT_lang_10()
