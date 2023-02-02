5f5a183829791bd719fc21cc21c683abf4be58e2
Yuri UTKIN <yu-2023@inbox.lv>
2023-02-02 13:24:15 +0200

    Add files via upload

---
+++ src/main/kotlin/lv/yu/kot/lang/KOT_lang_09.kt
@@ -0,0 +1,29 @@
+
+/**
+ * 
+ * Kotlin package     lv.yu.kot.lang
+ * 
+ * Kotlin program     KOT_lang_09.kt     Apache License 2.0
+ *      
+ * Copyright (c)      Yuri Utkin 2023    mob.+371 12345678     https://www.jago.lv
+ * 
+ */
+package lv.yu.kot.lang
+
+fun KOT_lang_09() {
+    	
+    lv.yu.kot.start.KOT_start_menu_09()
+    
+	lv.yu.kot.start.KOT_start_menu_start_09()
+    lv.yu.kot.load.KOT_load_menu_start_09()
+    lv.yu.kot.xxxxx.KOT_xxxxx_menu_start_09()
+    
+	lv.yu.kot.lang.KOT_lang_menu_lang_09()
+	
+	lv.yu.kot.help.KOT_help_menu_help_09()
+	lv.yu.kot.lang.KOT_lang_menu_lang_09()
+    lv.yu.kot.exit.KOT_exit_menu_exit_09()
+	
+	lv.yu.kot.frame_09.setJMenuBar(lv.yu.kot.menubar_09)
+
+}  //  end KOT_lang_09()
---
+++ src/main/kotlin/lv/yu/kot/lang/KOT_lang_visualize_09.kt
@@ -0,0 +1,23 @@
+
+/**
+ * 
+ * Kotlin package     lv.yu.kot.lang
+ * 
+ * Kotlin program     KOT_lang_visualize_09.kt     Apache License 2.0
+ *      
+ * Copyright (c)      Yuri Utkin 2023              mob.+371 12345678     https://www.jago.lv
+ * 
+ */
+package lv.yu.kot.lang
+
+fun KOT_lang_visualize_09() {
+
+//--------------------------------------------------    	
+    	
+        lv.yu.kot.lang.KOT_lang_09()
+		     		
+//--------------------------------------------------          
+			            		
+        lv.yu.kot.frame_09.isVisible = true
+
+}  //  end KOT_lang_visualize_09()
